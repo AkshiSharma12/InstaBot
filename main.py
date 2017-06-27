397096448b09fa129da2f2fe2537dfd4091c57f6
akshi <akshisharma12@gmail.com>
2017-06-27 11:56:24 +0530

    InstaBot Project

--- main.py
+++ main.py
@@ -18,7 +18,52 @@ def self_info():
             print 'User does not exist!'
     else:
         print 'Status code other than 200 received!'
-self_info()
+
+def get_user_id(insta_username):
+    request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_info = requests.get(request_url).json()
+
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']):
+            return user_info['data'][0]['id']
+        else:
+            return None
+    else:
+        print 'Status code other than 200 received!'
+        exit()
 
 
+def get_own_post():
+    request_url = (BASE_URL + 'users/self/media/recent/?access_token=%s') % (APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    own_media = requests.get(request_url).json()
 
+    if own_media['meta']['code'] == 200:
+     if len(own_media['data']):
+      return own_media['data'][0]['id']
+     else:
+      print 'Post does not exist!'
+    else:
+      print 'Status code other than 200 received!'
+    return None
+
+def get_user_post(insta_username):
+  user_id = get_user_id(insta_username)
+  if user_id == None:
+    print 'User does not exist!'
+    exit()
+  request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+  print 'GET request url : %s' % (request_url)
+  user_media = requests.get(request_url).json()
+  if user_media['meta']['code'] == 200:
+      if len(user_media['data']):
+          return user_media['data'][0]['id']
+      else:
+          print "There is no recent post!"
+  else:
+      print "Status code other than 200 received!"
+  return None
+
+
+get_own_post()
